84e40ef29cb6122aa73872ee56ec416d9bbea434
khalish Luthfi <khalishluthfi@gmail.com>
2022-10-14 02:20:31 +0800

    add table and remove column

--- migrations/20221013181343-create-like.js
+++ migrations/20221013181343-create-like.js
@@ -2,17 +2,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Comments", {
+    await queryInterface.createTable("Likes", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      content: {
-        type: Sequelize.STRING,
-      },
-      like: {
         type: Sequelize.INTEGER,
       },
       UserId: {
@@ -38,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Comments");
+    await queryInterface.dropTable("Likes");
   },
 };
